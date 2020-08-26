25983b40e1ea1c74b24e01d294643a30f5719802
Foxugly <rvilain@skynet.be>
2020-08-26 16:56:24 +0200

    Update models.py

--- mycalendar/models.py
+++ mycalendar/models.py
@@ -76,7 +76,7 @@ class MyCalendar(models.Model):
 
     def get_ics_facebook_link(self):
         full_url = "%s://%s%s" % self.request.scheme, self.request.META.HTTP_HOST, self.ics.url)
-        url = "https://www.facebook.com/sharer/sharer.php?u=%s" % ( self.request.scheme}}{request.META.HTTP_HOST}}quote_plus(full_url)
+        url = "https://www.facebook.com/sharer/sharer.php?u=%s" % quote_plus(full_url)
         return url
 
     # <button class="btn btn-social-icon btn-facebook" type="button" onclick="dpisocial_share_this('https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.lesoir.be%2F321090%2Farticle%2F2020-08-26%2Fen-aout-une-personne-contaminee-sur-5-avait-voyage-selon-le-centre-de-crise', 'facebook', 321090);"><i class="fa fa-facebook icon icon-facebook"></i></button>
